e8ea24f83befe0934ce2a30c7f2cbae7080fd5bb
svenzaimovic <svenzaimovic@gmail.com>
2016-01-14 01:02:00 +0100

    Backend fix, final

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
-
+ 
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
